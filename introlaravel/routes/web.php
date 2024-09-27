0114fc1c5c4c0bce8278f4668173a4e986a2633c
Kevin15032 <kdtorres1503@gmail.com>
2024-09-26 19:11:04 -0600

    Avance26/09/24

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('inicio');
+// })->name('rutaInicio');
+
+// Route::get('/from', function () {
+//     return view('formulario');
+// })->name('rutaFrom');
+
+// Route::get('/consultar', function () {
+//     return view('cliente');
+// })->name('rutaClientes');
+
+Route::view('/','inicio')->name('rutaInicio');
+Route::view('/from','formulario')->name('rutaFrom');
+Route::view('/consultar','cliente')->name('rutaClientes');

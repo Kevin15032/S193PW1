e045f473735645aee673595bc33d1cacb6677340
Kevin15032 <kdtorres1503@gmail.com>
2024-10-10 18:04:44 -0600

    practica 3.1 10/10/24

--- introlaravel/resources/views/Componentes.blade.php
+++ introlaravel/resources/views/Componentes.blade.php
@@ -4,10 +4,12 @@
 
 @section('contenido')
 
-<x-card encabezado="Componente" titulo="Dinámico" textoBoton="Guardar"></x-card>
+<x-card encabezado="Componente" titulo="Dinámico" textoBoton="Guardar"> </x-card>
 
 <x-card encabezado="Componente 2" titulo="Dinámico 2" textoBoton="Guardar 2"></x-card>
 
+<x-alert tipo="danger">Rojo</x-alert>
+<x-alert tipo="warning">Amarillo</x-alert>
 
 
 @endsection

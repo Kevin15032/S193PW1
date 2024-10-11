7d598ea9bb8cba203389b0b480584b4e2eaf13fd
Kevin15032 <kdtorres1503@gmail.com>
2024-10-10 19:04:06 -0600

    practica 4 10/10/24

--- introlaravel/routes/web.php
+++ introlaravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ControladorVistas;
 
 // Route::get('/', function () {
 //     return view('inicio');
@@ -14,7 +15,13 @@ use Illuminate\Support\Facades\Route;
 //     return view('cliente');
 // })->name('rutaClientes');
 
-Route::view('/','inicio')->name('rutaInicio');
-Route::view('/from','formulario')->name('rutaFrom');
-Route::view('/consultar','cliente')->name('rutaClientes');
-Route::view('/componet','componentes')->name('rutaComponent');
+// Route::view('/','inicio')->name('rutaInicio');
+// Route::view('/from','formulario')->name('rutaFrom');
+// Route::view('/consultar','cliente')->name('rutaClientes');
+// Route::view('/componet','componentes')->name('rutaComponent');
+
+Route::get('/',[ControladorVistas::class,'home'])->name('rutaInicio');
+
+Route::get('/from',[ControladorVistas::class,'formulario'])->name('rutaFrom');
+
+Route::get('/consultar',[ControladorVistas::class,'consulta'])->name('rutaClientes');

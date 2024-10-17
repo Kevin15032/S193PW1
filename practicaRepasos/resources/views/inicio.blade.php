b8dbd7feef5bbabdb890f55e85126ceb1efe47ec
Kevin15032 <kdtorres1503@gmail.com>
2024-10-17 10:40:32 -0600

    botón REPASO 1 realizado

--- practicaRepasos/resources/views/inicio.blade.php
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -4,11 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Inicio</title>
+    @vite(['resources/js/app.js'])
 </head>
 <body>
+    <div class="container mt-5 col-md-6">
     <h1>Nombre:Domínguez Torres Kevin</h1>
     <h1>Grupo:s193</h1>
     <h1>Materia: Programacón web</h1>
+    <a href="{{ route('rutaRepaso') }}"  class="btn btn-primary">REPASO 1</a>
+     </div>
 </body>
 </html>
--- practicaRepasos/routes/web.php
+++ practicaRepasos/routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/','inicio')->name('rutaInicio');
+Route::view('/repaso','repaso1')->name('rutaRepaso');

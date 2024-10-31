dc8daa32f33f2e733b470d58a0fc7ef377c8c717
Kevin15032 <kdtorres1503@gmail.com>
2024-10-30 23:27:03 -0600

    Creacion de pagina noticia

--- RepasoII/resources/views/principal.blade.php
+++ RepasoII/resources/views/principal.blade.php
@@ -1,33 +1,22 @@
 @extends('layouts.platilla')
 @section('contenido')
-
-<h1>Libros, el pueblo español de 100 habitantes que pide ayuda para crear una biblioteca (y así evitar desaparecer)</h1>
-<div class="center-text">Hay un pueblo en la provincia de Teruel (España) cuyas únicas diez calles acogen sólo a 100 habitantes. Lleva por nombre Libros y en su urgencia por no desaparecer quiere convertirse en una ciudad literaria para atraer al turismo.</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div class="container my-4">
+    <div class="row align-items-center">
+        <div class="col-md-8">
+            <h1 class="text-start"> Libros, el pueblo español de 100 habitantes que pide ayuda para crear una biblioteca (y así evitar desaparecer)</h1>
+            <p>Hay un pueblo en la provincia de Teruel (España) cuyas únicas diez calles acogen sólo a 100 habitantes. Lleva por nombre Libros y en su urgencia por no desaparecer quiere convertirse en una ciudad literaria para atraer al turismo.</p>
+            <p>La mayoría de quienes viven ahí sobrepasan los 65 años. No existe generación de relevo, porque los más jóvenes se han ido a las ciudades por falta de trabajo. Y la única escuela tuvo que cerrar en 2012 por la falta de niños.</p>
+             <p>Florencio Gabalda (91 años) ha sido testigo de cómo Libros se ha ido despoblando. Llegó ahí hace medio siglo, cuando lo habitaban 500 personas. Se casó y formó una familia. Hoy es el único que queda de su núcleo, porque su hijo tuvo que irse a la ciudad de Teruel en busca de oportunidades. “Aquí no pudo ni estudiar”, lamenta.</p>
+             <p>De seguir así, a Libros le restan si acaso dos décadas antes de quedar como un pueblo fantasma. “Si no hacemos algo, estamos condenados a desaparecer”, afirma a BBC Mundo el alcalde de la localidad, Raúl Arana, quien con 35 años decidió apostar a quedarse en el pueblo de sus abuelos.</p>
+    </div>
+</div>
 <footer>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row text-center">
          <p>
-            © 2023 Biblioteca Municiapal de Queretaro. Todos los derechos reservados.
+            © 2023 Biblioteca de Queretaro. Todos los derechos reservados.
          </p>
+         </div>
+         </div>
 </footer>
 @endsection

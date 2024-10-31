f8650e06e9cd4395ee1309ce6588897a9ad7b7df
Kevin15032 <kdtorres1503@gmail.com>
2024-10-31 03:00:34 -0600

    Validador  finalizado en la funcion procesarLibro

--- RepasoII/resources/views/registrolibros.blade.php
+++ RepasoII/resources/views/registrolibros.blade.php
@@ -22,42 +22,57 @@
             <div class="mb-3">
                 <label for="isbn" class="form-label">ISBN:</label>
                 <input type="text" class="form-control" name="txtisbn" value="{{ old('txtisbn') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txtisbn')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="titulo" class="form-label">Titulo:</label>
                 <input type="text" class="form-control" name="txttitulo" value="{{ old('txttitulo') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txttitulo')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="autor" class="form-label">Autor:</label>
                 <input type="text" class="form-control" name="txtautor" value="{{ old('txtautor') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txtautor')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="paginas" class="form-label">Páginas:</label>
                 <input type="text" class="form-control" name="txtpaginas" value="{{ old('txtpaginas') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txtpaginas')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="año" class="form-label">Año:</label>
                 <input type="text" class="form-control" name="txtaño" value="{{ old('txtaño') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txtaño')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="editorial" class="form-label">Editorial:</label>
                 <input type="text" class="form-control" name="txteditorial" value="{{ old('txteditorial') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txteditorial')}}</small>
             </div>
 
             <div class="mb-3">
                 <label for="email" class="form-label">Email de Editorial:</label>
                 <input type="text" class="form-control" name="txtemail" value="{{ old('txtemail') }}">
+                <small class="text-danger fst-italic">{{$errors->first('txtemail')}}</small>
             </div>
 
             <button type="submit" class="btn btn-success btn-sm">Guardar Libro</button>
         </form>
     </div>
 </div>
-
+<footer>
+    <div class="container-fluid">
+        <div class="row text-center">
+         <p>
+            © 2023 Biblioteca de Queretaro. Todos los derechos reservados.
+         </p>
+         </div>
+         </div>
+</footer>
 
 @endsection
